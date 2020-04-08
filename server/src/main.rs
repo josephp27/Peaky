dd27704d4f4d34b545ab2135682b11c5f35b9e54
Thomas Procopio <thomasprocopio@Thomass-MacBook-Pro.local>
2020-04-08 16:16:06 -0500

    adds server that sends data to all connections but sender

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,30 @@
+use std::collections::HashSet;
+use std::net::UdpSocket;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = [0; 10];
+
+    let socket = UdpSocket::bind("127.0.0.1:34254").unwrap();
+    let mut clients = HashSet::new();
+    loop {
+        match socket.recv_from(&mut buf) {
+            Ok((amt, src)) => {
+                clients.insert(src);
+
+                for dest in clients.clone() {
+                    let cloned_socket = socket.try_clone().unwrap();
+
+                    if src != dest {
+                        thread::spawn(move || {
+                            cloned_socket.send_to(&mut buf[..amt], &dest).unwrap();
+                        });
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
