0cebe32795e485bfe945139f42ed74b8ea66524b
Joseph Procopio <josephp27@live.com>
2020-04-09 16:05:38 -0500

    test

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,20 +2,20 @@ extern crate scrap;
 
 use std::io::ErrorKind::WouldBlock;
 use std::io::Write;
+use std::net::UdpSocket;
 use std::process::{Command, Stdio};
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 
 use scrap::{Capturer, Display};
 
-fn main() {
-
-    let d = Display::primary().unwrap();
-    let (w, h) = (d.width() / 4, d.height() / 4);
-
+fn display(socket: UdpSocket, w: usize, h: usize) {
+    let socket = UdpSocket::bind("0.0.0.0:8082").unwrap();
     let child = Command::new("ffplay")
         .args(&[
             "-f", "rawvideo",
             "-alwaysontop",
-            "-pixel_format", "bgr0",
             "-video_size", &format!("{}x{}", w, h),
             "-framerate", "60",
             "-"
@@ -24,8 +24,29 @@ fn main() {
         .spawn()
         .expect("This example requires ffplay.");
 
-    let mut capturer = Capturer::new(d).unwrap();
     let mut out = child.stdin.unwrap();
+
+    let size = 8388608;
+    let mut buf: Box<[u8]> = vec![0; size].into_boxed_slice();
+
+    socket.send_to("start".as_bytes(), "18.220.60.51:8080").unwrap();
+    loop {
+        let (amt, _) = socket.recv_from(&mut buf).unwrap();
+        let buf = &mut buf[..amt];
+        out.write_all(buf).unwrap();
+    }
+}
+
+fn main() {
+    let d = Display::primary().unwrap();
+    let (w, h) = (d.width() / 2, d.height() / 2);
+
+    let mut capturer = Capturer::new(d).unwrap();
+
+    let socket = UdpSocket::bind("0.0.0.0:8081").unwrap();
+    let cloned_socket = socket.try_clone().unwrap();
+    thread::spawn(move || display(socket.try_clone().unwrap(), w, h));
+
 
     loop {
         match capturer.frame() {
@@ -34,12 +55,17 @@ fn main() {
                 let stride = frame.len() / h;
                 let rowlen = 4 * w;
 
-                println!("{:?}", frame.chunks(stride));
+                let mut red = [0; 819200];
+                let mut j = 0;
                 for row in frame.chunks(stride) {
-                    let row = &row[..rowlen];
-                    out.write_all(row).unwrap();
+                    for i in 0..rowlen {
+                        if i % 2 == 0 {
+                            red[j] = row[i];
+                            j += 1;
+                        }
+                    }
                 }
-                break;
+                cloned_socket.send_to(&red[..819200 / 1000], "18.220.60.51:8080").unwrap();
             }
             Err(ref e) if e.kind() == WouldBlock => {
                 // Wait for the frame.
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,7 +3,6 @@ use std::net::UdpSocket;
 use std::thread;
 
 fn main() {
-    let mut buf = [0; 1024];
 
     let addr = "0.0.0.0:8080";
     println!("listening on: {}", addr);
@@ -12,16 +11,19 @@ fn main() {
     let mut clients = HashSet::new();
 
     loop {
-        match socket.recv_from(&mut buf) {
+        let size = 8388608;
+        let mut buffer2: Box<[u8]> = vec![0; size].into_boxed_slice();
+        match socket.recv_from(&mut buffer2) {
             Ok((amt, src)) => {
                 clients.insert(src);
 
+                println!("{:?}", clients);
                 for dest in clients.clone() {
                     let cloned_socket = socket.try_clone().unwrap();
-
+                    let buffy = buffer2.clone();
                     if src != dest {
                         thread::spawn(move || {
-                            cloned_socket.send_to(&mut buf[..amt], &dest).unwrap();
+                            cloned_socket.send_to(&buffy[..amt], &dest).unwrap();
                         });
                     }
                 }
