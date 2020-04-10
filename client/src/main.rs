779fda590526c88657ad950d27e30cca308a6b1e
Joseph Procopio <josephp27@live.com>
2020-04-10 17:21:43 -0500

    drops packets but video playing isnt bad

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,64 +1,56 @@
-extern crate captrs;
-
-use std::io::ErrorKind::WouldBlock;
-use std::io::Write;
+use std::io::{BufWriter, Read, Write};
 use std::net::UdpSocket;
 use std::process::{Command, Stdio};
 use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 
-use captrs::{Bgr8, Capturer};
-
-
-fn display(socket: UdpSocket, w: u32, h: u32) {
-    let socket = UdpSocket::bind("0.0.0.0:8082").unwrap();
-    let child = Command::new("ffplay")
+fn display() {
+    let ffplay = Command::new("ffplay")
         .args(&[
-            "-f", "rawvideo",
+            "-analyzeduration", "100",
+            "-fflags", "nobuffer",
+            "-f", "mpegts",
             "-alwaysontop",
-            "-video_size", &format!("{}x{}", w, h),
-            "-framerate", "60",
             "-"
         ])
         .stdin(Stdio::piped())
         .spawn()
         .expect("This example requires ffplay.");
+    let mut input = ffplay.stdin.unwrap();
 
-    let mut out = child.stdin.unwrap();
-
-    let size = 8388608;
-    let mut buf: Box<[u8]> = vec![0; size].into_boxed_slice();
-
+    let socket = UdpSocket::bind("0.0.0.0:8082").unwrap();
     socket.send_to("start".as_bytes(), "18.220.60.51:8080").unwrap();
+
+    let mut a = [0 as u8; 1024];
     loop {
-        let (amt, _) = socket.recv_from(&mut buf).unwrap();
-        let buf = &mut buf[..amt];
-        out.write_all(buf).unwrap();
+        let (amt, _) = socket.recv_from(&mut a).unwrap();
+        input.write_all(&a).unwrap();
     }
 }
 
 fn main() {
-    let mut capturer = Capturer::new(0).unwrap();
+    let mut ffmpeg = Command::new("ffmpeg")
+        .args(&[
+            "-f", "avfoundation",
+            "-i", "1",
+            "-vf", "scale=256:128",
+            "-pixel_format", "brg0",
+            "-f", "mpegts",
+            "-"
+        ])
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("This example requires ffplay.");
+    let mut output = ffmpeg.stdout.unwrap();
 
-    let (w, h) = capturer.geometry();
-    let size = w as u64 * h as u64;
+    let socket = UdpSocket::bind("0.0.0.0:8081").unwrap();
+    let cloned_socket = socket.try_clone().unwrap();
+    thread::spawn(move || display());
 
-    // let socket = UdpSocket::bind("0.0.0.0:8081").unwrap();
-    // let cloned_socket = socket.try_clone().unwrap();
-    // thread::spawn(move || display(socket.try_clone().unwrap(), w, h));
-
-
+    let mut a = [0 as u8; 1024];
     loop {
-        let ps = capturer.capture_frame().unwrap();
-
-        let mut a = Vec::new();
-        for Bgr8 { r, .. } in ps.into_iter() {
-            a.push(r)
-        }
-
-        println!("{}", a.len());
-
-        // cloned_socket.send_to(&row[..4 * w], "18.220.60.51:8080").unwrap();
+        output.read(&mut a).unwrap();
+        cloned_socket.send_to(&a, "18.220.60.51:8080").unwrap();
     }
 }
