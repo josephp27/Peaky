b77a7a3a382dc4440188567c28bed905fc6cdbef
Joseph Procopio <josephp27@live.com>
2020-04-12 16:23:37 -0500

    switches to socket2 to increase receive buffer

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,21 @@
 use std::io::{Read, Write};
+use std::net::SocketAddr;
 use std::net::UdpSocket;
 use std::process::{Command, Stdio};
 use std::thread;
 
+use socket2::{Domain, Protocol, Socket, Type};
+
 const WIDTH: usize = 2560 / 8;
 const HEIGHT: usize = 1440 / 8;
-const BUFFER_SIZE: usize = WIDTH * HEIGHT / 2;
+const BUFFER_SIZE: usize = WIDTH * HEIGHT;
 
-fn display(socket: UdpSocket) {
+fn display(socket: Socket) {
+    let socky = Socket::new(Domain::ipv4(), Type::dgram(), Option::from(Protocol::udp())).unwrap();
+    socky.bind(&"0.0.0.0:8084".parse::<SocketAddr>().unwrap().into()).unwrap();
+    socky.set_recv_buffer_size(BUFFER_SIZE * 2);
+    socky.send_to("start".as_bytes(), &"18.188.172.124:8080".parse::<SocketAddr>().unwrap().into());
+
     let ffplay = Command::new("ffplay")
         .args(&[
             "-analyzeduration", "100",
@@ -25,9 +33,9 @@ fn display(socket: UdpSocket) {
 
     let mut input = ffplay.stdin.unwrap();
     let mut a = [0 as u8; BUFFER_SIZE];
+
     loop {
-        let socket = socket.try_clone().unwrap();
-        let (_, _) = socket.recv_from(&mut a).unwrap();
+        let (_, _) = socky.recv_from(&mut a).unwrap();
         input.write_all(&a).unwrap();
     }
 }
@@ -35,8 +43,8 @@ fn display(socket: UdpSocket) {
 fn main() {
     let mut ffmpeg = Command::new("ffmpeg")
         .args(&[
-            "-f", "dshow",
-            "-i", "video=screen-capture-recorder",
+            "-f", "avfoundation",
+            "-i", "1",
             "-vf", format!("scale={}:{}", WIDTH, HEIGHT).to_string().as_str(),
             "-pixel_format", "brg0",
             "-f", "mpegts",
@@ -47,13 +55,17 @@ fn main() {
         .expect("This example requires ffplay.");
     let mut output = ffmpeg.stdout.unwrap();
 
-    let socket = UdpSocket::bind("0.0.0.0:8084").unwrap();
+    let socket = Socket::new(Domain::ipv4(), Type::dgram(), Option::from(Protocol::udp())).unwrap();
+    socket.bind(&"0.0.0.0:8081".parse::<SocketAddr>().unwrap().into()).unwrap();
+    socket.set_send_buffer_size(BUFFER_SIZE * 2);
+
     let cloned_socket = socket.try_clone().unwrap();
+
     thread::spawn(move || display(socket));
 
     let mut a = [0 as u8; BUFFER_SIZE];
     loop {
         output.read(&mut a).unwrap();
-        cloned_socket.send_to(&a, "18.188.172.124:8080").unwrap();
+        cloned_socket.send_to(&a, &"18.188.172.124:8080".parse::<SocketAddr>().unwrap().into()).unwrap();
     }
 }
