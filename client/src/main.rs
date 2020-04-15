ed5b19b08ab4d60af58ed5fd355f699362208f8d
Joseph Procopio <josephp27@live.com>
2020-04-15 13:13:58 -0500

    adds threadpool

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,71 +1,40 @@
-use std::io::{Read, Write};
-use std::net::SocketAddr;
 use std::net::UdpSocket;
-use std::process::{Command, Stdio};
 use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 
-use socket2::{Domain, Protocol, Socket, Type};
+use threadpool::ThreadPool;
 
-const WIDTH: usize = 2560 / 8;
-const HEIGHT: usize = 1440 / 8;
-const BUFFER_SIZE: usize = WIDTH * HEIGHT;
+const BUFFER_SIZE: usize = 502;
+// const DESTINATION: &str = "18.188.172.124:8080";
+const DESTINATION: &str = "0.0.0.0:8080";
+const NUM_THREADS: usize = 10;
 
-fn display(socket: Socket) {
-    let socky = Socket::new(Domain::ipv4(), Type::dgram(), Option::from(Protocol::udp())).unwrap();
-    socky.bind(&"0.0.0.0:8084".parse::<SocketAddr>().unwrap().into()).unwrap();
-    socky.set_recv_buffer_size(BUFFER_SIZE * 2);
-    socky.send_to("start".as_bytes(), &"18.188.172.124:8080".parse::<SocketAddr>().unwrap().into());
-
-    let ffplay = Command::new("ffplay")
-        .args(&[
-            "-analyzeduration", "100",
-            "-fflags", "nobuffer",
-            "-f", "mpegts",
-            "-alwaysontop",
-            "-probesize", "320",
-            "-sync", "ext",
-            "-"
-        ])
-        .stdin(Stdio::piped())
-        .spawn()
-        .expect("This example requires ffplay.");
-
-
-    let mut input = ffplay.stdin.unwrap();
-    let mut a = [0 as u8; BUFFER_SIZE];
-
-    loop {
-        let (_, _) = socky.recv_from(&mut a).unwrap();
-        input.write_all(&a).unwrap();
+fn display(socket: UdpSocket) -> ThreadPool {
+    let pool = ThreadPool::new(20);
+    for _ in 0..20 {
+        let socket = socket.try_clone().unwrap();
+        pool.execute(move || {
+            let mut a = [0 as u8; BUFFER_SIZE];
+            loop {
+                let (_, _) = socket.recv_from(&mut a).unwrap();
+                println!("received");
+            }
+        });
     }
+
+    pool
 }
 
 fn main() {
-    let mut ffmpeg = Command::new("ffmpeg")
-        .args(&[
-            "-f", "avfoundation",
-            "-i", "1",
-            "-vf", format!("scale={}:{}", WIDTH, HEIGHT).to_string().as_str(),
-            "-pixel_format", "brg0",
-            "-f", "mpegts",
-            "-"
-        ])
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("This example requires ffplay.");
-    let mut output = ffmpeg.stdout.unwrap();
-
-    let socket = Socket::new(Domain::ipv4(), Type::dgram(), Option::from(Protocol::udp())).unwrap();
-    socket.bind(&"0.0.0.0:8081".parse::<SocketAddr>().unwrap().into()).unwrap();
-    socket.set_send_buffer_size(BUFFER_SIZE * 2);
-
+    let socket = UdpSocket::bind("0.0.0.0:8081").unwrap();
     let cloned_socket = socket.try_clone().unwrap();
 
-    thread::spawn(move || display(socket));
+    let pool = display(socket);
 
-    let mut a = [0 as u8; BUFFER_SIZE];
-    loop {
-        output.read(&mut a).unwrap();
-        cloned_socket.send_to(&a, &"18.188.172.124:8080".parse::<SocketAddr>().unwrap().into()).unwrap();
+    for _ in 0..40000 {
+        cloned_socket.send_to("send".as_bytes(), DESTINATION).unwrap();
     }
+
+    pool.join();
 }
