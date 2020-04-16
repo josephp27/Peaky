850e34681a2813b78dd720cbba51173b6b2e8970
Joseph Procopio <josephp27@live.com>
2020-04-16 12:39:15 -0500

    supports adding to array at fixed time and at a specific location

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,10 @@
 use std::cmp::min;
-use std::collections::VecDeque;
-use std::io::Write;
+use std::convert::TryInto;
 use std::net::UdpSocket;
-use std::ops::Deref;
-use std::process::{Command, Stdio};
 use std::str;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::thread::sleep;
-use std::time::Duration;
 
 use scrap::{Capturer, Display};
 use threadpool::ThreadPool;
-use std::ptr::{slice_from_raw_parts_mut, slice_from_raw_parts};
-use std::convert::TryInto;
 
 const BUFFER_SIZE: usize = 502;
 // const DESTINATION: &str = "18.188.172.124:8080";
@@ -25,18 +16,23 @@ fn pop(barry: &[u8]) -> [u8; 4] {
 }
 
 fn display(socket: UdpSocket) -> ThreadPool {
-    // let frame = Arc::new(Mutex::new(VecDeque::new()));
+    let frame = Box::new(vec![vec![]; 8192]);
     let pool = ThreadPool::new(NUM_THREADS + 1);
+
     for _ in 0..NUM_THREADS {
         let socket = socket.try_clone().unwrap();
-        // let frame = frame.clone();
+        let mut new_ = (*frame).clone();
+
         pool.execute(move || {
-            let mut a = [0 as u8; BUFFER_SIZE + 2];
+            let mut a = vec![0 as u8; BUFFER_SIZE + 4];
             loop {
                 let (_, _) = socket.recv_from(&mut a).unwrap();
-                println!("{:?}",  u32::from_be_bytes(pop(&a[..4])));
+                let packet_num = u32::from_be_bytes(pop(&a[..4]));
+                let x = &a[4..BUFFER_SIZE + 4];
 
-                // frame.lock().unwrap().push_front(a);
+                // let x = &mut a[4..];
+                new_.insert(packet_num as usize, a[4..].to_vec());
+                println!("{:?} {:?}", packet_num, new_[packet_num as usize]);
             }
         });
     }
@@ -86,7 +82,7 @@ fn main() {
                 let end = i + min(BUFFER_SIZE, frame.len() - i);
 
                 let mut buffer = packet_sequence.to_be_bytes().to_vec();
-                let mut data: &[u8] = &frame[i..end];
+                let data: &[u8] = &frame[i..end];
                 buffer.append(&mut data.to_vec());
 
                 cloned_socket.send_to(&buffer, DESTINATION).unwrap();
@@ -96,7 +92,5 @@ fn main() {
             break;
         }
     }
-    loop {
-
-    }
+    loop {}
 }
