1c4b04ab86d32a0b27e355135d02a243e299a557
Joseph Procopio <josephp27@live.com>
2020-04-20 13:49:40 -0500

    uses 2 queues to process a frame

--- client/src/main.rs
+++ client/src/main.rs
@@ -24,30 +24,15 @@ fn pop(barry: &[u8]) -> [u8; 4] {
 
 fn display(socket: UdpSocket) -> ThreadPool {
     let k = KeyComparator(|k: &(u32, Vec<u8>)| Reverse(k.0));
+    let k2 = KeyComparator(|j: &(u32, Vec<u8>)| Reverse(j.0));
     let frame = Arc::new(Mutex::new(BinaryHeap::from_vec_cmp(vec![], k)));
+    let frame2 = Arc::new(Mutex::new(BinaryHeap::from_vec_cmp(vec![], k2)));
     let pool = ThreadPool::new(NUM_THREADS + 1);
+    let curr_queue = Arc::new(Mutex::new(0));
 
-    for _ in 0..NUM_THREADS {
-        let socket = socket.try_clone().unwrap();
-        let frame = frame.clone();
-
-        pool.execute(move || {
-            let mut a = vec![0 as u8; BUFFER_SIZE + 4];
-            loop {
-                let (_, _) = socket.recv_from(&mut a).unwrap();
-
-                let packet_num = u32::from_be_bytes(pop(&a[..4]));
-                let data = a[4..].to_vec();
-
-                // println!("{}", packet_num);
-
-                let mut guard = frame.lock().unwrap();
-                guard.push((packet_num, data));
-                // println!("{} {}", packet_num, guard.len());
-            }
-        });
-    }
-
+    let frame_display = frame.clone();
+    let frame_display2 = frame2.clone();
+    let curr_queue_display = curr_queue.clone();
     pool.execute(move || {
         let child = Command::new("ffplay")
             .args(&[
@@ -63,16 +48,51 @@ fn display(socket: UdpSocket) -> ThreadPool {
             .expect("This example requires ffplay.");
 
         let mut out = child.stdin.unwrap();
+        sleep(Duration::from_millis(100));
         loop {
-            sleep(Duration::from_millis(1000));
-            let mut guard1 = frame.lock().unwrap();
-            // if guard1.len() > 5000 {
-                for i in 0..guard1.len() {
-                    out.write_all(&guard1.pop().unwrap().1);
+            if *curr_queue_display.lock().unwrap() == 1 {
+                let mut guard1 = frame_display.lock().unwrap();
+                let len = guard1.len();
+                for _ in 0..len {
+                    out.write_all(&guard1.pop().unwrap().1).unwrap();
                 }
-            // }
+            } else {
+                let mut guard2 = frame_display2.lock().unwrap();
+                let len = guard2.len();
+                for _ in 0..len {
+                    out.write_all(&guard2.pop().unwrap().1).unwrap();
+                }
+            }
         }
     });
+
+    for _ in 0..NUM_THREADS {
+        let socket = socket.try_clone().unwrap();
+        let frame = frame.clone();
+        let frame2 = frame2.clone();
+        let curr_queue = curr_queue.clone();
+
+        pool.execute(move || {
+            let mut a = vec![0 as u8; BUFFER_SIZE + 5];
+            loop {
+                let (_, _) = socket.recv_from(&mut a).unwrap();
+
+                let packet_num = u32::from_be_bytes(pop(&a[..4]));
+                let queue_num = a[4];
+                let data = a[5..].to_vec();
+                *curr_queue.lock().unwrap() = queue_num;
+
+                if queue_num == 1 {
+                    let mut guard2 = frame2.lock().unwrap();
+                    guard2.push((packet_num, data));
+                } else {
+                    let mut guard = frame.lock().unwrap();
+                    guard.push((packet_num, data));
+                }
+            }
+        });
+    }
+
 
     pool
 }
@@ -86,21 +106,25 @@ fn main() {
     let d = Display::primary().unwrap();
     let mut capturer = Capturer::new(d).unwrap();
 
+    let mut queue_num: u8 = 0;
     loop {
-        let mut packet_sequence: u32 = 0;
         if let Ok(frame) = capturer.frame() {
+            let mut packet_sequence: u32 = 0;
+            println!("{}", frame.len());
             for i in (0..frame.len()).step_by(BUFFER_SIZE) {
                 let end = i + min(BUFFER_SIZE, frame.len() - i);
 
                 let mut buffer = packet_sequence.to_be_bytes().to_vec();
                 let data: &[u8] = &frame[i..end];
+                buffer.append(&mut vec![queue_num]);
                 buffer.append(&mut data.to_vec());
 
-                cloned_socket.send_to(&buffer, DESTINATION).unwrap();
+                // cloned_socket.send_to(&buffer, DESTINATION).unwrap();
 
                 packet_sequence += 1;
             }
-            sleep(Duration::from_millis(20000));
+            sleep(Duration::from_millis(3000));
+            queue_num ^= 1;
         }
     }
 }
