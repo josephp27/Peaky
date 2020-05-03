d350cdd4456741e5d7aa1ddc26860662c8bac193
Joseph Procopio <josephp27@live.com>
2020-05-03 17:14:28 -0500

    renames marshal to listen

--- client/src/display.rs
+++ client/src/display.rs
@@ -3,7 +3,7 @@ use std::thread;
 
 use crate::utils::constants::NUM_THREADS;
 use crate::utils::draw::draw;
-use crate::utils::listener::marshal;
+use crate::utils::listener::listen;
 use crate::utils::settings::Settings;
 
 pub fn display_orchestrator(socket: UdpSocket, settings: Settings) {
@@ -11,6 +11,6 @@ pub fn display_orchestrator(socket: UdpSocket, settings: Settings) {
 
     (0..NUM_THREADS).for_each(|_| {
         let socket = socket.try_clone().unwrap();
-        thread::spawn(move || marshal(socket));
+        thread::spawn(move || listen(socket));
     });
 }
