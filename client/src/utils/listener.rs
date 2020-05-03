24d18e46dddeda7938f58bf2bb9f06e45d5cbdf4
Joseph Procopio <josephp27@live.com>
2020-05-03 18:21:24 -0500

    uses mpsc for message passing between threads

--- client/src/utils/listener.rs
+++ client/src/utils/listener.rs
@@ -1,8 +1,10 @@
+use std::net::UdpSocket;
+use std::sync::mpsc::Sender;
+
 use crate::utils::constants::{BUFFER_SIZE, PACKET_NUM_SIZE, QUEUE_NUM_SIZE};
 use crate::utils::helper;
-use std::net::UdpSocket;
 
-pub fn listen(socket: UdpSocket) {
+pub fn listen(socket: UdpSocket, tx: Sender<Vec<u8>>) {
     let mut a = vec![0 as u8; BUFFER_SIZE + PACKET_NUM_SIZE + QUEUE_NUM_SIZE];
     loop {
         let (_, _) = socket.recv_from(&mut a).unwrap();
@@ -10,5 +12,7 @@ pub fn listen(socket: UdpSocket) {
         let packet_num = helper::get_packet_num(&a[..4]);
         let queue_num = a[4];
         let data = a[5..].to_vec();
+
+        tx.send(data).unwrap();
     }
 }
