a64ee1a1bd2e7a52bfaec8e3fe0912b096c27756
Joseph Procopio <josephp27@live.com>
2020-05-03 19:54:52 -0500

    output slides left

--- client/src/capture.rs
+++ client/src/capture.rs
@@ -1,12 +1,11 @@
+use std::net::UdpSocket;
 
 use scrap::{Capturer, Display};
 
 use crate::utils::constants::{BUFFER_SIZE, DESTINATION};
 use crate::utils::helper::{build_buffer, calculate_frame_end};
-use std::net::UdpSocket;
 
 pub fn capture_orchestrator(primary_display: Display, socket: UdpSocket) {
-    let mut queue_num = 0;
     let mut capturer = Capturer::new(primary_display).unwrap();
 
     loop {
@@ -14,7 +13,7 @@ pub fn capture_orchestrator(primary_display: Display, socket: UdpSocket) {
             let mut packet_sequence: u32 = 0;
             for i in (0..frame.len()).step_by(BUFFER_SIZE) {
                 let end = calculate_frame_end(i, frame.len());
-                let buffer = build_buffer(packet_sequence, &frame[i..end], queue_num);
+                let buffer = build_buffer(packet_sequence, &frame[i..end]);
 
                 socket.send_to(&buffer, DESTINATION).unwrap();
                 packet_sequence += 1;
