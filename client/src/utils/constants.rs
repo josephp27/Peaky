06f963b39f293a2d5d90249e40030f38255a8eb7
Joseph Procopio <josephp27@live.com>
2020-05-10 17:16:02 -0500

    aligns video with socket

--- client/src/utils/constants.rs
+++ client/src/utils/constants.rs
@@ -2,4 +2,4 @@ pub const BUFFER_SIZE: usize = 7200;
 pub const PACKET_NUM_SIZE: usize = 4;
 pub const NUM_THREADS: usize = 10;
 pub const DESTINATION: &str = "127.0.0.1:8080";
-pub const RECEIVER: &str = "127.0.0.1:8085";
+pub const RECEIVER: &str = "127.0.0.1:8090";
--- client/src/utils/draw.rs
+++ client/src/utils/draw.rs
@@ -9,7 +9,6 @@ use crate::utils::settings::Settings;
 pub fn draw(settings: Settings, rx: Receiver<Vec<u8>>) {
     let mut canvas = get_canvas(settings.width, settings.height, settings.scalar);
 
-    let frame_size = (settings.height * settings.width * 4 / BUFFER_SIZE) + 1;
     let mut buffer: Vec<Vec<u8>> = vec![vec![0; BUFFER_SIZE]; 1116];
     loop {
         let data = rx.recv().unwrap();
@@ -18,7 +17,6 @@ pub fn draw(settings: Settings, rx: Receiver<Vec<u8>>) {
             for packet in buffer.clone() {
                 canvas.write_all(&packet);
             }
-            // buffer = vec![vec![0; BUFFER_SIZE]; 1116];
         } else {
             let index = helper::get_packet_num(&data[..4]);
             let mut vec1 = data[4..].to_vec();
