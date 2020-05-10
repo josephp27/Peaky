da547a6888648533b76daaee1343634247b80f14
Joseph Procopio <josephp27@live.com>
2020-05-10 17:16:02 -0500

    aligns video with socket

--- client/src/capture.rs
+++ client/src/capture.rs
@@ -1,7 +1,10 @@
+extern crate rand;
+
 use std::net::UdpSocket;
 use std::sync::mpsc;
 use std::thread;
 
+use rand::Rng;
 use scrap::{Capturer, Display};
 
 use crate::utils::constants::{BUFFER_SIZE, DESTINATION};
@@ -9,35 +12,21 @@ use crate::utils::draw::draw;
 use crate::utils::helper::{build_buffer, calculate_frame_end};
 use crate::utils::settings::Settings;
 
-extern crate rand;
 
-use rand::Rng;
-
-pub fn capture_orchestrator(primary_display: Display, settings: Settings) {
+pub fn capture_orchestrator(primary_display: Display, socket: UdpSocket) {
     let mut capturer = Capturer::new(primary_display).unwrap();
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        draw(settings, rx);
-    });
-    let mut rng = rand::thread_rng();
 
     loop {
         if let Ok(frame) = capturer.frame() {
             let mut packet_sequence: u32 = 0;
             for i in (0..frame.len()).step_by(BUFFER_SIZE) {
-                let val = rng.gen_range(1, 10);
-                if packet_sequence % val == 0 {
-                    packet_sequence += 1;
-                    continue;
-                }
                 let end = calculate_frame_end(i, frame.len());
                 let buffer = build_buffer(packet_sequence, &frame[i..end]);
 
-                // socket.send_to(&buffer, DESTINATION).unwrap();
-                tx.send(buffer).unwrap();
+                socket.send_to(&buffer, DESTINATION).unwrap();
                 packet_sequence += 1;
             }
-            tx.send("done".as_bytes().to_vec()).unwrap();
+            socket.send_to(&"done".as_bytes().to_vec(), DESTINATION).unwrap();
         }
     }
 }
--- client/src/utils/listener.rs
+++ client/src/utils/listener.rs
@@ -6,8 +6,8 @@ use crate::utils::constants::{BUFFER_SIZE, PACKET_NUM_SIZE};
 pub fn listen(socket: UdpSocket, tx: Sender<Vec<u8>>) {
     loop {
         let mut buffer = vec![0 as u8; BUFFER_SIZE + PACKET_NUM_SIZE];
-        let (_, _) = socket.recv_from(&mut buffer).unwrap();
+        let (amt, src) = socket.recv_from(&mut buffer).unwrap();
 
-        tx.send(buffer).unwrap();
+        tx.send(buffer[..amt].to_vec()).unwrap();
     }
 }
