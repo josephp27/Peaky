aad70089086612b8fff61b03963e542b9962e58b
Joseph Procopio <josephp27@live.com>
2020-05-16 21:22:46 -0500

    changes to amazon ip

--- client/src/utils/constants.rs
+++ client/src/utils/constants.rs
@@ -1,5 +1,5 @@
 pub const BUFFER_SIZE: usize = 7200;
 pub const PACKET_NUM_SIZE: usize = 4;
 pub const NUM_THREADS: usize = 10;
-pub const DESTINATION: &str = "127.0.0.1:8080";
+pub const DESTINATION: &str = "3.22.130.243:8081";
 pub const RECEIVER: &str = "127.0.0.1:8090";
