8ca1fe7c0e49c1e34898d228168edef26b4f018f
Joseph Procopio <josephp27@live.com>
2020-10-04 18:54:56 -0500

    adds fps limiter to sender

--- client/src/capture.rs
+++ client/src/capture.rs
@@ -1,8 +1,10 @@
 extern crate rand;
 
 use std::net::UdpSocket;
+use std::os::raw::c_float;
 use std::sync::mpsc;
 use std::thread;
+use std::time::{Duration, Instant};
 
 use rand::Rng;
 use scrap::{Capturer, Display};
@@ -12,13 +14,17 @@ use crate::utils::draw::draw;
 use crate::utils::helper::{build_buffer, calculate_frame_end};
 use crate::utils::settings::Settings;
 
-
 pub fn capture_orchestrator(primary_display: Display, socket: UdpSocket) {
     let mut capturer = Capturer::new(primary_display).unwrap();
+    let frame_max_runtime = 1. / 25.;
+
 
     loop {
+        let start = Instant::now();
+
         if let Ok(frame) = capturer.frame() {
             let mut packet_sequence: u32 = 0;
+
             for i in (0..frame.len()).step_by(BUFFER_SIZE) {
                 let end = calculate_frame_end(i, frame.len());
                 let buffer = build_buffer(packet_sequence, &frame[i..end]);
@@ -26,7 +32,10 @@ pub fn capture_orchestrator(primary_display: Display, socket: UdpSocket) {
                 socket.send_to(&buffer, DESTINATION).unwrap();
                 packet_sequence += 1;
             }
-            socket.send_to(&"done".as_bytes().to_vec(), DESTINATION).unwrap();
         }
+
+        let time_left = frame_max_runtime - (start.elapsed().as_millis() as f64);
+        let duration = time_left.max(0.);
+        thread::sleep(Duration::from_millis((duration * 1000.) as u64));
     }
 }
